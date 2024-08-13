b510c06f1ef58015575072ea123ad3c4ba169b92
lgaieta <lgaieta.dev@gmail.com>
2024-08-13 12:10:38 -0300

    Added error handling in get product list

--- src/app/api/product/list/route.ts
+++ src/app/api/product/list/route.ts
@@ -2,7 +2,20 @@ import ProductRepository from "@/models/ProductRepository";
 import MockProductRepository from "@/services/MockProductRepository";
 
 export async function GET(request: Request) {
-    const productRepository: ProductRepository = new MockProductRepository();
-    const list = await productRepository.getList();
-    return Response.json(list);
+    try {
+        const productRepository: ProductRepository =
+            new MockProductRepository();
+        const list = await productRepository.getList();
+        return Response.json(list);
+    } catch (error) {
+        console.error("Error fetching the product list:", error);
+        return new Response(
+            JSON.stringify({
+                message: "An error occurred while fetching the product list.",
+            }),
+            {
+                status: 500,
+            },
+        );
+    }
 }
