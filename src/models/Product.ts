a0bac6a3368e53b3dd79e4a1b71c8b13986ed1b3
lgaieta.dev <lgaieta.dev@gmail.com>
2024-08-13 19:31:25 +0000

    Added more properties to Product and ProductRepository

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,6 +1,9 @@
 interface Product {
     id: number;
     name: string;
+    price: number;
+    description: string;
+    stock: number;
 }
 
 export default Product
--- src/models/ProductRepository.ts
+++ src/models/ProductRepository.ts
@@ -1,7 +1,9 @@
 import Product from "@/models/Product";
 
 interface ProductRepository {
+    getById(id: Product["id"]): Promise<Product | null>;
     getList(): Promise<Product[]>;
+    create(product: Product): Promise<Product>
 }
 
 export default ProductRepository;
