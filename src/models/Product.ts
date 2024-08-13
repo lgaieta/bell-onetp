2aef7b16c6627c881f46b317bdd7e3175c766f7e
lgaieta.dev <lgaieta.dev@gmail.com>
2024-08-13 21:15:34 +0000

    Working with validations and errors

--- src/models/Product.ts
+++ src/models/Product.ts
@@ -1,9 +1,15 @@
-interface Product {
-    id: number;
-    name: string;
-    price: number;
-    description: string;
-    stock: number;
-}
+import { z } from "zod";
+
+export const ProductIdSchema = z.coerce.number().min(1);
+
+export const ProductSchema = z.object({
+    id: ProductIdSchema,
+    name: z.string().min(1).max(100),
+    description: z.string().min(1).max(255),
+    price: z.number(),
+    stock: z.number(),
+});
+
+type Product = z.infer<typeof ProductSchema>;
 
 export default Product;
