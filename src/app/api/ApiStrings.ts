156d539666193d6f6a0e243961ec86be0c93080e
lgaieta <lgaieta.dev@gmail.com>
2024-08-14 12:27:19 -0300

    Added ApiStrings with generateResponseError

---
+++ src/app/api/ApiStrings.ts
@@ -0,0 +1,16 @@
+const ApiStrings = {
+    consoleProductPostError: "Error creating the product",
+    invalidFieldsMessage: "One or more fields are not valid.",
+    productCreationErrorMessage:
+        "An error occurred while creating the product.",
+    productNotFoundMessage: "Product not found",
+    invalidIdMessage: "Id is not valid.",
+    consoleProductFetchError: "Error fetching the product:",
+    productFetchErrorMessage: "An error occurred while fetching the product.",
+    consoleProductListFetchError: "Error fetching the product list:",
+    productListFetchErrorMessage:
+        "An error occurred while fetching the product list.",
+    productIdKey: "id",
+};
+
+export default ApiStrings;
--- src/app/api/product/GET.ts
+++ src/app/api/product/GET.ts
@@ -1,3 +1,4 @@
+import ApiStrings from "@/app/api/ApiStrings";
 import NotFoundError from "@/lib/NotFoundError";
 import { generateResponseError } from "@/lib/utils";
 import { ProductIdSchema } from "@/models/Product";
@@ -9,7 +10,7 @@ import { ZodError } from "zod";
 export async function GET(request: NextRequest) {
     try {
         const searchParams = request.nextUrl.searchParams;
-        const queryId = searchParams.get("id");
+        const queryId = searchParams.get(ApiStrings.productIdKey);
 
         const validatedId = ProductIdSchema.parse(queryId);
 
@@ -17,20 +18,23 @@ export async function GET(request: NextRequest) {
             new MockProductRepository();
         const product = await productRepository.getById(validatedId);
 
-        if (!product) throw new NotFoundError("Product not found");
+        if (!product)
+            throw new NotFoundError(ApiStrings.productNotFoundMessage);
 
         return Response.json(product);
     } catch (error) {
         if (error instanceof ZodError)
-            return generateResponseError(error.format());
+            return generateResponseError({
+                message: ApiStrings.invalidIdMessage,
+            });
 
         if (error instanceof NotFoundError)
-            return generateResponseError({ message: "Product not found" });
+            return generateResponseError({ message: error.message });
 
-        console.error("Error fetching the product:", error);
+        console.error(ApiStrings.consoleProductFetchError, error);
 
         return generateResponseError({
-            message: "An error occurred while fetching the product.",
+            message: ApiStrings.productFetchErrorMessage,
         });
     }
 }
