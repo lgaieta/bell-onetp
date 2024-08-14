50f3b83613dde521cb29599a5a0d8853f8efea70
lgaieta <lgaieta.dev@gmail.com>
2024-08-14 13:37:57 -0300

    Added DELETE handler to product route

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -1,3 +1,4 @@
 export { GET } from "@/app/api/product/GET";
 export { POST } from "@/app/api/product/POST";
 export { PUT } from "@/app/api/product/PUT";
+export { DELETE } from "@/app/api/product/DELETE";
