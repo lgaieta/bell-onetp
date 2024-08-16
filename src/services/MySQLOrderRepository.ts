f07844e0034b546c481be91526b441725e8eadef
lgaieta <lgaieta.dev@gmail.com>
2024-08-16 16:29:33 -0300

    Added getOrderById and getOrderByUsername

--- src/services/MySQLOrderRepository.ts
+++ src/services/MySQLOrderRepository.ts
@@ -26,89 +26,120 @@ interface DBOrderHasProduct extends RowDataPacket {
     amount_prod: number;
 }
 
-function adaptOrder(item: DBOrder): Omit<Order, "products"> {
-    return {
-        id: item.idorder,
-        username: item.user_order,
-        totalPrice: item.total_price,
-        operationState: item.operation_state as OrderState,
-        creationTime: item.create_time,
-    };
-}
-
-function adaptOrderHasProduct(
-    products: DBOrderHasProduct[],
-): Order["products"] {
-    return products.reduce(
-        (accumulator, current) => ({
-            ...accumulator,
-            [current.product_idproduct]: current.amount_prod,
-        }),
-        {},
-    );
-}
-
 class MySQLOrderRepository implements OrderRepository {
     async create(order: Order): Promise<Order> {
         const sql =
-            "INSERT INTO orders (user_order, create_time, total_price, operation_state) VALUES (?, ?, ?, ?)";
+            "INSERT INTO orders (user_order, total_price, operation_state) VALUES (?, ?, ?)";
         const [result] = await MySQLPool.get().query<ResultSetHeader>(sql, [
             order.username,
-            order.creationTime,
             order.totalPrice,
             order.operationState,
         ]);
         return { ...order, id: result.insertId };
     }
 
+    async delete(id: Order["id"]): Promise<void> {
+        const sql = "DELETE FROM orders WHERE idorder = ?";
+        await MySQLPool.get().query<ResultSetHeader>(sql, [id]);
+    }
+
     async update(newOrder: Order): Promise<Order> {
+        const sql =
+            "UPDATE orders SET total_price = ?, operation_state = ? WHERE idorder = ?";
+        await MySQLPool.get().query<ResultSetHeader>(sql, [
+            newOrder.totalPrice,
+            newOrder.operationState,
+            newOrder.id,
+        ]);
+
         return newOrder;
     }
 
     async getByUsername(username: User["username"]): Promise<Order[]> {
-        const sql = "SELECT * FROM orders WHERE username";
-        return [];
+        const sql = "SELECT * FROM orders WHERE user_order = ?";
+        const [dbOrders] = await MySQLPool.get().query<DBOrder[]>(sql, [
+            username,
+        ]);
+        const [dbOrderHasProducts] = await MySQLPool.get().query<
+            DBOrderHasProduct[]
+        >(
+            "SELECT r.order_idorder, r.product_idproduct, r.amount_prod FROM order_has_product AS r JOIN orders ON orders.user_order = ?",
+            [username],
+        );
+
+        return this.formatRelationship({ dbOrders, dbOrderHasProducts });
     }
 
     async getList(): Promise<Order[]> {
         const sql = "SELECT * FROM orders";
-        const [result] = await MySQLPool.get().query<DBOrder[]>(sql);
+        const [dbOrders] = await MySQLPool.get().query<DBOrder[]>(sql);
 
-        const [productsResult] = await MySQLPool.get().query<
+        const [dbOrderHasProducts] = await MySQLPool.get().query<
             DBOrderHasProduct[]
         >("SELECT product_idproduct FROM order_has_product");
 
+        return this.formatRelationship({ dbOrders, dbOrderHasProducts });
+    }
+
+    async getById(id: Order["id"]): Promise<Order> {
+        const sql = "SELECT * FROM orders WHERE idorder = ?";
+        const [result] = await MySQLPool.get().query<DBOrder[]>(sql, [id]);
+        if (result.length < 1)
+            throw new NotFoundError(ApiStrings.orderNotFoundMessage);
+
+        const adaptedOrder = this.adaptOrder(result[0]);
+        const [productsResult] = await MySQLPool.get().query<
+            DBOrderHasProduct[]
+        >("SELECT * FROM order_has_product WHERE order_idorder = ?", [id]);
+
+        const adaptedProducts = this.adaptOrderHasProduct(productsResult);
+
+        return { ...adaptedOrder, products: adaptedProducts };
+    }
+
+    private adaptOrder(item: DBOrder): Omit<Order, "products"> {
+        return {
+            id: item.idorder,
+            username: item.user_order,
+            totalPrice: item.total_price,
+            operationState: item.operation_state as OrderState,
+            creationTime: item.create_time,
+        };
+    }
+
+    private adaptOrderHasProduct(
+        products: DBOrderHasProduct[],
+    ): Order["products"] {
+        return products.reduce(
+            (accumulator, current) => ({
+                ...accumulator,
+                [current.product_idproduct]: current.amount_prod,
+            }),
+            {},
+        );
+    }
+
+    private formatRelationship({
+        dbOrders,
+        dbOrderHasProducts,
+    }: {
+        dbOrders: DBOrder[];
+        dbOrderHasProducts: DBOrderHasProduct[];
+    }) {
         const adaptedList: Order[] = [];
 
-        result.forEach((order) => {
-            const adaptedOrder = adaptOrder(order);
-            const matchingProducts = productsResult.filter(
+        dbOrders.forEach((order) => {
+            const adaptedOrder = this.adaptOrder(order);
+            const matchingProducts = dbOrderHasProducts.filter(
                 (rel) => rel.order_idorder === adaptedOrder.id,
             );
-            const adaptedProducts = adaptOrderHasProduct(matchingProducts);
+            const adaptedProducts = this.adaptOrderHasProduct(matchingProducts);
 
             adaptedList.push({ ...adaptedOrder, products: adaptedProducts });
         });
 
         return adaptedList;
     }
-
-    async getById(id: Order["id"]): Promise<Order> {
-        const sql = "SELECT * FROM orders WHERE idorder = ?";
-        const [result] = await MySQLPool.get().query<DBOrder[]>(sql, [id]);
-        if (result[0]) throw new NotFoundError(ApiStrings.orderNotFoundMessage);
-
-        const adaptedOrder = adaptOrder(result[0]);
-        const [productsResult] = await MySQLPool.get().query<
-            DBOrderHasProduct[]
-        >("SELECT *s FROM order_has_product WHERE order_idorder = ?", [id]);
-
-        const adaptedProducts = adaptOrderHasProduct(productsResult);
-
-        return { ...adaptedOrder, products: adaptedProducts };
-    }
-
-    async delete(id: Order["id"]): Promise<void> {}
 }
 
 export default MySQLOrderRepository;
