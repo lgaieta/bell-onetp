4165823b5aed79c4096fcfdb1903e0158920957d
lgaieta <lgaieta.dev@gmail.com>
2024-08-16 11:37:19 -0300

    Modified buttons on cart

--- src/components/PendingOrdersPage.tsx
+++ src/components/PendingOrdersPage.tsx
@@ -1,7 +1,7 @@
 import ProductRepository from "@/models/ProductRepository";
 import MockProductRepository from "@/services/MockProductRepository";
 
-async function PendingProductsPage() {
+async function PendingOrdersPage() {
     const productRepository: ProductRepository = new MockProductRepository();
     const list = await productRepository.getList();
 
@@ -12,26 +12,24 @@ async function PendingProductsPage() {
                     Productos Pendientes
                 </h1>
             </header>
-            <div className="flex w-full flex-col gap-y-10 px-4 max-w-sm sm:max-w-xl sm:flex-row sm:justify-between ">
-                <div className="flex flex-col w-full gap-6 sm:max-w-xs ">
+            <div className="flex w-full flex-col gap-y-10 p-4 bg-neutral-100 max-w-sm sm:max-w-xl sm:flex-row sm:justify-between ">
+                <ul className="flex flex-col w-full gap-6 sm:max-w-xl ">
                     {list.map((item) => (
-                        <div
+                        <li
                             key={item.id}
-                            className="flex flex-row justify-between p-6 rounded-lg bg-slate-100 font-semibold sm:text-sm sm:p-4 "
+                            className="flex flex-col justify-between p-6 rounded-lg  font-semibold  sm:text-sm sm:p-4 sm:flex-row "
                         >
-                            {item.name}
-                            <div className="flex flex-row gap-x-6">
+                            <p>{item.name}</p>
+                            <div className="flex gap-3">
                                 <p>{item.price} $</p>
-                                <button className="font-medium rounded-md px-2 text-red-500 hover:text-white hover:bg-red-600 ">
-                                    x
-                                </button>
+                                <p>ejemplo@gmail.com</p>
                             </div>
-                        </div>
+                        </li>
                     ))}
-                </div>
+                </ul>
             </div>
         </main>
     );
 }
 
-export default PendingProductsPage;
+export default PendingOrdersPage;
