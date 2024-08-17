e95037efbc79edca933f77d5d767aae4e45bf948
lgaieta <lgaieta.dev@gmail.com>
2024-08-16 21:30:05 -0300

    Added styles for header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,34 +1,72 @@
 import { GrCart } from "react-icons/gr";
 import { FaRegUser } from "react-icons/fa";
+import { MdMenu } from "react-icons/md";
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+
+const links = [
+    ["/productos", "Productos"],
+    ["/", "Sobre nosotros"],
+    ["/pedidos/pendientes", "Mis compras"],
+];
 
 function Header() {
     return (
-        <header className="flex w-full bg-zinc-800 justify-between items-center p-3">
-            <Link href="/" className="text-lg text-white font-bold ">
-                bell
-            </Link>
-            <div className="flex gap-4">
-                <Link
-                    href="/pedidos/pendientes"
-                    className="text-sm text-white font-bold py-1 px-2 rounded-sm transition delay-100 hover:bg-white hover:text-black "
-                >
-                    Pedidos pendientes
+        <header className="bg-background text-foreground sticky top-0 w-full border-b border-b-border">
+            <div className="flex gap-8 items-center justify-between md:justify-normal sm:container w-full px-4 h-14">
+                <Link href="/" className="tracking-tight text-lg font-bold">
+                    bell
                 </Link>
-            </div>
-            <div className="flex gap-x-2">
-                <Link
-                    href="/carrito"
-                    className="text-white p-2  rounded-sm transition delay-100 hover:bg-white hover:text-black "
-                >
-                    <GrCart />
-                </Link>
-                <Link
-                    href="/"
-                    className="text-white p-2  rounded-sm transition delay-100 hover:bg-white hover:text-black "
-                >
-                    <FaRegUser />
-                </Link>
+                <nav className="hidden md:flex">
+                    {links.map(([link, label]) => (
+                        <Button
+                            variant="link"
+                            asChild
+                            className="text-foreground font-normal"
+                            key={label}
+                        >
+                            <Link href={link}>{label}</Link>
+                        </Button>
+                    ))}
+                </nav>
+                <div className="hidden md:flex gap-2 justify-end w-full">
+                    <Button asChild variant="ghost" size="icon">
+                        <Link href="/carrito">
+                            <GrCart size={24} />
+                        </Link>
+                    </Button>
+                    <Button asChild variant="ghost" size="icon">
+                        <Link href="/">
+                            <FaRegUser size={24} />
+                        </Link>
+                    </Button>
+                </div>
+                <Sheet>
+                    <SheetTrigger>
+                        <Button
+                            className="md:hidden"
+                            variant="outline"
+                            size="icon"
+                        >
+                            <MdMenu size={24} />
+                        </Button>
+                    </SheetTrigger>
+                    <SheetContent>
+                        <nav className="flex flex-col">
+                            {links.map(([link, label]) => (
+                                <Button
+                                    variant="link"
+                                    asChild
+                                    className="text-foreground py-8 w-full"
+                                    key={label}
+                                >
+                                    <Link href={link}>{label}</Link>
+                                </Button>
+                            ))}
+                        </nav>
+                    </SheetContent>
+                </Sheet>
             </div>
         </header>
     );
