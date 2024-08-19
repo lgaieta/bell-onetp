9af702b39232d6854fac1486b67a701a704a8612
lgaieta <lgaieta.dev@gmail.com>
2024-08-19 20:42:06 -0300

    Fixed main error page styles

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -2,11 +2,11 @@
 
 export default function ErrorPage() {
     return (
-        <main className="flex flex-col gap-6 py-16 w-full max-w-sm px-4">
-            <h1 className="text-2xl font-bold text-center">
+        <main className="flex flex-col gap-6 py-16 w-full items-center px-4">
+            <h1 className="text-2xl font-bold text-center max-w-sm">
                 Ha ocurrido un error
             </h1>
-            <p className="text-center">
+            <p className="text-center max-w-sm">
                 Si el error continúa, puede contactarse con atención al cliente.
             </p>
         </main>
