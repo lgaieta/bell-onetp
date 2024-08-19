535a37ab6d750bd55eedc7a17b0e4ffe36bb8f64
lgaieta <lgaieta.dev@gmail.com>
2024-08-19 14:09:17 -0300

    Added styles in CreateProductPage

--- src/components/CreateProductPage.tsx
+++ src/components/CreateProductPage.tsx
@@ -8,8 +8,8 @@ type CreateProductPageProps = {
 
 function CreateProductPage(props: CreateProductPageProps) {
     return (
-        <main className="flex flex-col justify-center items-center px-4 w-full sm:container">
-            <header className="flex justify-center py-16">
+        <main className="flex flex-col gap-12 justify-center items-center py-24 px-4 w-full sm:container">
+            <header className="flex justify-center">
                 <h1 className="text-2xl font-bold sm:text-4xl">
                     Cargar producto
                 </h1>
