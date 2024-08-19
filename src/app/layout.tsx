d872c958d2c5071e9eb8fbab4710583d1ea13fa4
lgaieta <lgaieta.dev@gmail.com>
2024-08-19 20:05:39 -0300

    Added min height to screens

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
         <html lang="en">
             <body className={inter.className}>
                 <Header />
-                {children}
+                <div className="min-h-screen">{children}</div>
                 <Footer />
             </body>
         </html>
