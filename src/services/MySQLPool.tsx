e4343f15f6bdc0598f56ad3abd434d7c7d304ba2
lgaieta <lgaieta.dev@gmail.com>
2024-08-20 11:37:21 -0300

    Too many connections test

--- src/services/MySQLPool.tsx
+++ src/services/MySQLPool.tsx
@@ -6,6 +6,13 @@ export const MySQLPool: Pool = mysql.createPool({
     password: process.env.DB_PASSWORD || "1234",
     database: process.env.DB_NAME || "onetp",
     port: +process.env.DB_PORT!,
+    waitForConnections: true,
+    connectionLimit: 10,
+    maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+    idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0,
 });
 
 export default MySQLPool;
