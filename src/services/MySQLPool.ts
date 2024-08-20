a6a48d1a585c9f369f7f5d48eec0f533818bc16d
lgaieta <lgaieta.dev@gmail.com>
2024-08-20 16:11:22 -0300

    Removed old mysql pool

--- src/services/MySQLPool.ts
+++ src/services/MySQLPool.ts
@@ -1,21 +1,18 @@
 import mysql, { Pool } from "mysql2/promise";
 
-let pool: Pool | undefined = undefined;
-
-class MySQLPool {
-    static get() {
-        if (pool) return pool;
-
-        pool = mysql.createPool({
-            host: process.env.DB_HOST || "localhost",
-            user: process.env.DB_USER || "root",
-            password: process.env.DB_PASSWORD || "1234",
-            database: process.env.DB_NAME || "onetp",
-            port: +process.env.DB_PORT!,
-        });
-
-        return pool;
-    }
-}
+export const MySQLPool: Pool = mysql.createPool({
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "1234",
+    database: process.env.DB_NAME || "onetp",
+    port: +process.env.DB_PORT!,
+    waitForConnections: true,
+    connectionLimit: 200,
+    maxIdle: 200, // max idle connections, the default value is the same as `connectionLimit`
+    idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0,
+});
 
 export default MySQLPool;
