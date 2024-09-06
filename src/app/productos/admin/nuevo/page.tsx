e7097f27c61e33dc50ece007e6872cb6f8f5546d
lgaieta <lgaieta.dev@gmail.com>
2024-09-05 22:22:05 -0300

    Added authorization for creating products

--- src/app/productos/admin/nuevo/page.tsx
+++ src/app/productos/admin/nuevo/page.tsx
@@ -1,10 +1,16 @@
 import CreateProductPage from "@/components/create-product/CreateProductPage";
+import SessionManager from "@/services/SessionManager";
+import { SessionType } from "@/services/SessionPayload";
 import { Metadata } from "next";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
     title: "Cargar producto - bell",
 };
 
 export default async function Page() {
+    const { isAuth, type } = await SessionManager.verifySession();
+    if (!isAuth || type !== SessionType.Admin) redirect("/");
+
     return <CreateProductPage />;
 }
